07a10b077ea062c5936b6cf491ddc748244401c2
Hiago75 <hiagobrenha@protonmail.com>
2021-07-02 15:01:37 -0300

    Convert post content from MD to HTML

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -9,7 +9,12 @@ export type DynamicPostProps = {
 };
 
 export default function DynamicPost({ post }: DynamicPostProps) {
-  return <p>{post.title}</p>;
+  return (
+    <>
+      <p>{post.title}</p>
+      <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+    </>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
