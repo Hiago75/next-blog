f8c0ceeff78bea12a6c5ed10f51b10316c180bae
Hiago75 <hiagobrenha75@gmail.com>
2021-09-07 18:22:40 -0300

    chore: update edit page to profile page

---
+++ src/pages/admin/dashboard/profile.tsx
@@ -0,0 +1,34 @@
+import { GetServerSideProps } from 'next';
+import { destroyCookie } from 'nookies';
+import { useContext } from 'react';
+import { DashboardContainer } from '../../../components';
+import { EditUser } from '../../../containers/';
+import { AuthContext } from '../../../contexts/AuthContext';
+
+export default function AdminHome() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <DashboardContainer>
+      <EditUser user={user} />
+    </DashboardContainer>
+  );
+}
+
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const { refresh_token, isAuthenticated } = ctx.req.cookies;
+
+//   if (!refresh_token) {
+//     isAuthenticated ? destroyCookie(ctx, 'isAuthenticated') : '';
+//     return {
+//       redirect: {
+//         destination: '/admin',
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: {},
+//   };
+// };
