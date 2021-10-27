249e0210d947fb9dc50a533754a452dcf64a9895
Hiago75 <hiagobrenha75@gmail.com>
2021-10-27 20:55:51 -0300

    refactor(next.config.js): add the "generateBuildId" option

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   target: 'serverless',
+  generateBuildId: () => 'build',
   env: {
     API_URL: process.env.API_URL,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
